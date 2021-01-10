d87fd6b6e5d850e19e072bb896b5e34b4cbeb3ad
uu <846821463@qq.com>
2021-01-10 16:11:57 +0800

    1.构造方法

---
+++ untitled/src/Student.java
@@ -0,0 +1,10 @@
+public class Student extends User{
+    private String major;
+    private String phone;
+
+    public String getMajor(){return major;}
+    public  void setMajor(String major){this.major=major;}
+    public String getPhone(){return phone;}
+    public void setPhone(String phone){this.phone}
+
+}
